5c8bd4af5cb7e67c18f337174d65466a9752efda
Aathish <63100842+theaathish@users.noreply.github.com>
2025-06-12 22:13:31 +0530

    feat: added resend and nodemailer

--- src/app/api/contact-resend/route.ts
+++ src/app/api/contact-resend/route.ts
@@ -1,3 +1,9 @@
-export default function ContactResend() {
-  return null;
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.json({ message: 'This endpoint is not used' }, { status: 404 });
+}
+
+export async function POST() {
+  return NextResponse.json({ message: 'This endpoint is not used' }, { status: 404 });
 }
